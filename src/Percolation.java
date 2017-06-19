b103cc0a597b5183530218e339d3e6667b61ef16
PsiAmp <kostinkostya@gmail.com>
2017-06-20 00:24:15 +0300

    made percolation work

--- src/Percolation.java
+++ src/Percolation.java
@@ -4,7 +4,7 @@ public class Percolation {
 
     private static final int[][] shift = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     private static final int FULL = 0;
-    public static final int OPEN = 1;
+    private static final int OPEN = 1;
     private int[][] data;
     private int openPoints = 0;
     private WeightedQuickUnionUF union;
@@ -12,54 +12,42 @@ public class Percolation {
     public Percolation(int n) {
         if (n <= 0) throw new IllegalArgumentException();
         data = new int[n][n];
-        union = new WeightedQuickUnionUF(data.length);
+        union = new WeightedQuickUnionUF(n*n);
+    }
+
+    private boolean indexesOutOfBounds(int row, int col) {
+        return row <= 0 || col <= 0 || row > data.length || col > data.length;
     }
 
     private void validate(int row, int col) {
-        row--;
-        col--;
-        if (row < 0 || col < 0 || row > data.length-1 || col > data.length-1) {
-            throw new IndexOutOfBoundsException();
-        }
+        if (indexesOutOfBounds(row, col)) throw new IndexOutOfBoundsException();
     }
 
     public void open(int row, int col) {
-        validate(row, col);
-        row--;
-        col--;
-        if (data[row][col] != OPEN) {
-            data[row][col] = OPEN;
+        if (!isOpen(row, col)) {
+            data[row-1][col-1] = OPEN;
             openPoints++;
             for (int i = 0; i < shift.length; i++) {
-                int shiftRow = shift[i][0];
-                int shiftColumn = shift[i][1];
-                union();
+                int shiftRow = row + shift[i][0];
+                int shiftColumn = col + shift[i][1];
+                if (!indexesOutOfBounds(shiftRow, shiftColumn) && isOpen(shiftRow, shiftColumn))
+                    union.union(getIndex(row, col), getIndex(shiftRow, shiftColumn));
             }
-            union(index, index - 1);
-            union(index, index + 1);
-            union(index, index - dimension);
-            union(index, index + dimension);
         }
     }
 
     private int getIndex(int row, int col) {
-        return row
-    }
-
-    private void union(int a, int b) {
-        if (b >= 0 && b < data.length) {
-            union.union(a, b);
-        }
+        return (row-1) * data.length + col - 1;
     }
 
     public boolean isOpen(int row, int col) {
         validate(row, col);
-        return data[row--][col--] == OPEN;
+        return data[row-1][col-1] == OPEN;
     }
 
     public boolean isFull(int row, int col) {
         validate(row, col);
-        return data[row--][col--] == FULL;
+        return data[row-1][col-1] == FULL;
     }
 
     public int numberOfOpenSites() {
@@ -67,9 +55,11 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[i].length; j++) {
-                if (union.connected(i, data.length - j - 1)) return true;
+        for (int i = 1; i <= data.length; i++) {
+            for (int j = 1; j <= data.length; j++) {
+                if (union.connected(getIndex(1, i), getIndex(data.length, j))) {
+                    return true;
+                }
             }
         }
         return false;
